c855064e924cae029fa9428caae04d9292e00679
Amen Ra <amenra@USRMonitors-MacBook-Pro.local>
2019-11-18 22:55:01 -0500

    complete user signup backend

--- backend/models/user.js
+++ backend/models/user.js
@@ -33,7 +33,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true
     },
-    salt: Number,
+    salt: String,
     about: {
       type: String
     },
@@ -53,4 +53,38 @@ const userSchema = new mongoose.Schema(
   { timestamp: true }
 );
 
-module.exports = mongoose.model('User', userSchema);
+userSchema
+  .virtual("password")
+  .set(function(password) {
+    // create a temporary variable called _password
+    this._password = password;
+    // generate a salt
+    this.salt = this.makeSalt();
+    // encrypt password
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+userSchema.methods = {
+  authenticate: function(plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+  encryptPassword: function(password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (error) {
+      return "";
+    }
+  },
+  makeSalt: function() {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  }
+};
+
+module.exports = mongoose.model("User", userSchema);
