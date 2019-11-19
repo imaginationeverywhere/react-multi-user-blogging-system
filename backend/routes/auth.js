9e6fcab759a075e6bed16a94629e2e854871c1ee
Amen Ra <amenra@USRMonitors-MacBook-Pro.local>
2019-11-18 21:58:17 -0500

    adding validation to user signup

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const { signup } = require("../contollers/auth");
 const router = express.Router();
 
-router.post("/signup", signup);
+// validators
+const { runValidation } = require("../validators");
+const { userSiignupValidator } = require("../validators/auth");
+
+router.post("/signup", userSiignupValidator, runValidation, signup);
 
 module.exports = router;
