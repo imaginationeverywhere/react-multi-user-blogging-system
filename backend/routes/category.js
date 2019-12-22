efb8035dc20aed3673f49464d42e9ec0d4e8b8a2
Amen Ra <amenra@USRMonitors-MacBook-Pro.local>
2019-12-22 16:34:58 -0500

    implimented categories

---
+++ backend/routes/category.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+// auth controllers
+const { requireSignin, adminMiddleware } = require("../contollers/auth");  
+// category controllers
+const { create, list, read, remove } = require("../contollers/category");
+// validators
+const { runValidation } = require("../validators");
+// category validators
+const {categroyCreateValidator } = require("../validators/category");
+
+
+router.post("/category", categroyCreateValidator, runValidation, requireSignin, adminMiddleware, create);
+router.get("/categories", list);
+router.get("/category/:slug", read);
+router.delete("/category/:slug", requireSignin, adminMiddleware, remove);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,51 +1,52 @@
-const express = require ('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-// blog routes
-const blogRoutes = require('./routes/blog');
-
-// auth routes
-const authRoutes = require('./routes/auth');
-
-// user routes
-const userRoutes = require('./routes/user');
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // app
 const app = express();
 
 // db
 mongoose
-    .connect(process.env.DATABASE_LOCAL, {
-        useNewUrlParser: true,  
-        useUnifiedTopology: true, 
-        useCreateIndex: true, 
-        useFindAndModify: false 
-    })
-    .then(() => console.log('DB connected'));
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log("DB connected"));
 
 // middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 // cors
-if(process.env.NODE_ENV === 'development'){
-    app.use(cors({origin: `${process.env.CLIENT_URL}`}));
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
 
+// blog routes
+const blogRoutes = require("./routes/blog");
+// auth routes
+const authRoutes = require("./routes/auth");
+// user routes
+const userRoutes = require("./routes/user");
+// category routes
+const categoryRoutes = require("./routes/category");
+
 // routes middleware
-app.use('/api', blogRoutes);
-app.use('/api', authRoutes);
-app.use('/api', userRoutes);
+app.use("/api", blogRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
 
 // port
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
