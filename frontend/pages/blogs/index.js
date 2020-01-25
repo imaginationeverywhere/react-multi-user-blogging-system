d0dab20d1cf5cb3827082e726b172a34c9042141
Amen Ra <amenra@USRMonitors-MacBook-Pro.local>
2020-01-25 15:27:01 -0500

    adding link, meta, and title tags for the Head section of the blog list page

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -1,18 +1,49 @@
 import Head from "next/head";
 import Link from "next/link";
+import { withRouter } from "next/router";
 import Layout from "../../components/Layout";
 import { useState } from "react";
-import { API } from "../../config";
+import { API, DOMAIN, APP_NAME, FB_APP_ID } from "../../config";
 import { listBlogsWithCategoriesAndTags } from "../../actions/blog";
 import Card from "../../components/blog/Card";
 
-const Blogs = ({ blogs, categories, tags, size }) => {
+const Blogs = ({ blogs, categories, tags, size, router }) => {
+  const head = () => (
+    <Head>
+      <title>Programming blogs | {APP_NAME}</title>
+      <meta
+        name="description"
+        content="Programming and tutorials about react, node, next, and redux."
+      />
+      <link rel="cannical" href={`${DOMAIN}${router.pathname}`} />
+      <meta
+        property="og:title"
+        content={`Latest web development tutorials | ${APP_NAME}`}
+      />
+      <meta
+        property="og:description"
+        content="Programming and tutorials about react, node, next, and redux."
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={`${DOMAIN}${router.pathname}`} />
+      <meta property="og:site_name" content={`${APP_NAME}`} />
+
+      <meta property="og:image" content={`${DOMAIN}/static/images/diversityblog.jpg`} />
+      <meta
+        property="og:image:secure_url"
+        content={`${DOMAIN}/static/images/diversityblog.jpg`}
+      />
+      <meta property="og:image:type" content="image/jpg" />
+      <meta property="fb:app_id" content={`${FB_APP_ID}`} />
+    </Head>
+  );
+
   const showAllBlogs = () => {
     return blogs.map((blog, i) => {
       return (
         <article key={i}>
           <Card blog={blog} />
-          <hr/>
+          <hr />
         </article>
       );
     });
@@ -21,49 +52,48 @@ const Blogs = ({ blogs, categories, tags, size }) => {
   const showAllCategories = () => {
     return categories.map((c, i) => (
       <Link href={`/categories/${c.slug}`} key={i}>
-        <a className="btn btn-primary mr-1 ml-1 mt-3">
-          {c.name}
-        </a>
+        <a className="btn btn-primary mr-1 ml-1 mt-3">{c.name}</a>
       </Link>
-    ))
-  }
+    ));
+  };
 
   const showAllTags = () => {
     return tags.map((t, i) => (
       <Link href={`/tags/${t.slug}`} key={i}>
-        <a className="btn btn-outline-primary mr-1 ml-1 mt-3">
-          {t.name}
-        </a>
+        <a className="btn btn-outline-primary mr-1 ml-1 mt-3">{t.name}</a>
       </Link>
-    ))
-  }
+    ));
+  };
 
   return (
-    <Layout>
-      <main>
-        <div className="contaner-fluid">
-          <header>
-            <div className="col-md-12-pt-3">
-              <h1 className="display-4 font-weight-bold text-center">
-                Diversity Developers Blogs and Tutorials
-              </h1>
-            </div>
-            <section>
-              <div className="pb-5 text-center">
-                {showAllCategories()}
-                <br />
-                {showAllTags()}
+    <React.Fragment>
+      {head()}
+      <Layout>
+        <main>
+          <div className="contaner-fluid">
+            <header>
+              <div className="col-md-12-pt-3">
+                <h1 className="display-4 font-weight-bold text-center">
+                  Diversity Developers Blogs and Tutorials
+                </h1>
               </div>
-            </section>
-          </header>
-        </div>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-12">{showAllBlogs()}</div>
+              <section>
+                <div className="pb-5 text-center">
+                  {showAllCategories()}
+                  <br />
+                  {showAllTags()}
+                </div>
+              </section>
+            </header>
           </div>
-        </div>
-      </main>
-    </Layout>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-12">{showAllBlogs()}</div>
+            </div>
+          </div>
+        </main>
+      </Layout>
+    </React.Fragment>
   );
 };
 
@@ -82,4 +112,4 @@ Blogs.getInitialProps = () => {
   });
 };
 
-export default Blogs;
+export default withRouter(Blogs);
