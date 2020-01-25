b00cd439a959223333992336ab49439b0e4dcc40
Amen Ra <amenra@USRMonitors-MacBook-Pro.local>
2020-01-25 11:43:14 -0500

    showing all categories and tags at the top of the blog listing page

--- frontend/pages/blogs/index.js
+++ frontend/pages/blogs/index.js
@@ -18,6 +18,26 @@ const Blogs = ({ blogs, categories, tags, size }) => {
     });
   };
 
+  const showAllCategories = () => {
+    return categories.map((c, i) => (
+      <Link href={`/categories/${c.slug}`} key={i}>
+        <a className="btn btn-primary mr-1 ml-1 mt-3">
+          {c.name}
+        </a>
+      </Link>
+    ))
+  }
+
+  const showAllTags = () => {
+    return tags.map((t, i) => (
+      <Link href={`/tags/${t.slug}`} key={i}>
+        <a className="btn btn-outline-primary mr-1 ml-1 mt-3">
+          {t.name}
+        </a>
+      </Link>
+    ))
+  }
+
   return (
     <Layout>
       <main>
@@ -29,7 +49,11 @@ const Blogs = ({ blogs, categories, tags, size }) => {
               </h1>
             </div>
             <section>
-              <p>show categories and tags</p>
+              <div className="pb-5 text-center">
+                {showAllCategories()}
+                <br />
+                {showAllTags()}
+              </div>
             </section>
           </header>
         </div>
