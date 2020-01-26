4c0ee0d82bf20ae89f6f1c8bf211233528b3a861
Amen Ra <amenra@USRMonitors-MacBook-Pro.local>
2020-01-25 19:35:20 -0500

    implimented link, title, and meta tags for single blog page

--- frontend/pages/blogs/[slug].js
+++ frontend/pages/blogs/[slug].js
@@ -4,10 +4,33 @@ import Layout from "../../components/Layout";
 import { useState } from "react";
 import { singleBlog } from "../../actions/blog";
 import { API, DOMAIN, APP_NAME, FB_APP_ID } from "../../config";
-import renderHTML from 'react-render-html';
-import moment from 'moment';
+import renderHTML from "react-render-html";
+import moment from "moment";
 
-const SingleBlog = ({ blog }) => {
+const SingleBlog = ({ blog, query }) => {
+  const head = () => (
+    <Head>
+      <title>
+        {blog.title} | {APP_NAME}
+      </title>
+      <meta name="description" content={blog.mdesc} />
+      <link rel="canonical" href={`${DOMAIN}/blogs/${query.slug}`} />
+      <meta property="og:title" content={`${blog.title}| ${APP_NAME}`} />
+      <meta property="og:description" content={blog.mdesc} />
+      <meta property="og:type" content="webiste" />
+      <meta property="og:url" content={`${DOMAIN}/blogs/${query.slug}`} />
+      <meta property="og:site_name" content={`${APP_NAME}`} />
+
+      <meta property="og:image" content={`${API}/blog/photo/${blog.slug}`} />
+      <meta
+        property="og:image:secure_url"
+        ccontent={`${API}/blog/photo/${blog.slug}`}
+      />
+      <meta property="og:image:type" content="image/jpg" />
+      <meta property="fb:app_id" content={`${FB_APP_ID}`} />
+    </Head>
+  );
+
   const showBlogCategories = blog =>
     blog.categories.map((c, i) => (
       <Link key={i} href={`/categories/${c.slug}`}>
@@ -24,6 +47,7 @@ const SingleBlog = ({ blog }) => {
 
   return (
     <React.Fragment>
+      {head()}
       <Layout>
         <main>
           <article>
@@ -39,16 +63,21 @@ const SingleBlog = ({ blog }) => {
               </section>
 
               <section>
-                <p className="lead mt-3 mark">
-                  Written by {blog.postedBy.name} | Published{" "}
-                  {moment(blog.updatedAt).fromNow()}
-                </p>
+                <div className="container">
+                  <h1 className="display-2 pb-3 pt-3 text-center font-weight-bold">
+                    {blog.title}
+                  </h1>
+                  <p className="lead mt-3 mark">
+                    Written by {blog.postedBy.name} | Published{" "}
+                    {moment(blog.updatedAt).fromNow()}
+                  </p>
 
-                <div className="pb-3">
-                  {showBlogCategories(blog)}
-                  {showBlogTags(blog)}
-                  <br />
-                  <br />
+                  <div className="pb-3">
+                    {showBlogCategories(blog)}
+                    {showBlogTags(blog)}
+                    <br />
+                    <br />
+                  </div>
                 </div>
               </section>
             </div>
@@ -81,7 +110,7 @@ SingleBlog.getInitialProps = ({ query }) => {
       console.log(data.error);
     } else {
       // console.log('GET INITIAL PROPS IN SINGLE BLOG', data);
-      return { blog: data };
+      return { blog: data, query };
     }
   });
 };
