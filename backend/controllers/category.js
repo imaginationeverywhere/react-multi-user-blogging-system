43110e5b29862e553b0115f6c7fd1e5a37f37383
Amen Ra <amenra@USRMonitors-MacBook-Pro.local>
2020-02-28 00:28:49 -0500

    adding single category page and associative blogs

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -1,4 +1,5 @@
 const Category = require("../models/category");
+const Blog = require("../models/blog");
 const slugify = require("slugify");
 const { errorHandler } = require("../helpers/dbErrorhandler");
 
@@ -36,7 +37,22 @@ exports.read = (req, res) => {
         error: errorHandler(err)
       });
     }
-    res.json(category);
+    // res.json(category);
+    Blog.find({ categories: category })
+      .populate("categories", "_id name slug")
+      .populate("tags", "_id name slug")
+      .populate("postedBy", "_id name")
+      .select(
+        "_id title slug excerpt categories postedBy tags createdAt updatedAt"
+      )
+      .exec((err, data) => {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err)
+          });
+        }
+        res.json({ category: category, blogs: data });
+      });
   });
 };
 exports.remove = (req, res) => {
@@ -48,7 +64,7 @@ exports.remove = (req, res) => {
       });
     }
     res.json({
-        message: "Category deleted successfully"
+      message: "Category deleted successfully"
     });
   });
 };
