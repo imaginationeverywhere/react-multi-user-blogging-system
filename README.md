1331fd21ce88560b771c20d399a6681774c49fb1
Amen Ra <amenra@USRMonitors-MacBook-Pro.local>
2020-03-01 19:48:41 -0500

    create backend public profile endpoint

--- README.md
+++ README.md
@@ -5,11 +5,12 @@
 ## Features
 
 1. Seach
-2. User Profile
-3. User Blog CRUD
-4. Disqus Commenting System
-5. Contact Form
-6. Forget/Reset Password
-7. email confirmation on signup
-8. login with Google
-9. Deploy to aws
+2. User Public Profile
+3. User Private Profile
+4. User Blog CRUD
+5. Disqus Commenting System
+6. Contact Form
+7. Forget/Reset Password
+8. email confirmation on signup
+9. login with Google
+10. Deploy to aws
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,10 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 // user controllers
-const { requireSignin, authMiddleware, adminMiddleware } = require("../controllers/auth");
-const { read } = require("../controllers/user");
+const {
+  requireSignin,
+  authMiddleware,
+  adminMiddleware
+} = require("../controllers/auth");
+const { read, publicProfile } = require("../controllers/user");
 
 router.get("/profile", requireSignin, authMiddleware, read);
+router.get("/user/:username", publicProfile);
 router.get("/admin", requireSignin, adminMiddleware, read);
 
 module.exports = router;
